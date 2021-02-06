1aed09af2ec575360ada7c4422c1fd63b96d9355
Haruki Nazawa <nazawaharuki@gmail.com>
2021-02-06 09:33:59 +0900

    serverのベースを作成

---
+++ server/server.go
@@ -0,0 +1,36 @@
+package server
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	libp2p "github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p-core/network"
+	host "github.com/libp2p/go-libp2p-host"
+)
+
+type Server struct {
+	node host.Host
+	addr string
+	port int16
+}
+
+// New create server
+func New(addr string, port int16) Server {
+	ctx := context.Background()
+	listenAddr := fmt.Sprintf("/ip4/%s/tcp/%d", addr, port)
+	node, err := libp2p.New(ctx, libp2p.ListenAddrStrings(listenAddr))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	
+	return Server{node: node, addr: addr, port: port}
+}
+
+// Listen when it receives a value from the other node, and calls the handler.
+func (s *Server) Listen(handler network.StreamHandler) {
+	log.Println("Waiting for client to connect.")
+	s.node.SetStreamHandler("/libp2p-port-forward/v0", handler)
+	return
+}
