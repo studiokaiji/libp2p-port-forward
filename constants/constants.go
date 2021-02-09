55faa24af9d8590049441ee45e62cc8e1961563f
Haruki Nazawa <nazawaharuki@gmail.com>
2021-02-09 18:05:07 +0900

    add BootstapPeers and Protocol

---
+++ constants/constants.go
@@ -0,0 +1,13 @@
+package constants
+
+import "github.com/multiformats/go-multiaddr"
+
+var BootstrapPeers = [5]multiaddr.Multiaddr{
+	multiaddr.StringCast("/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN"),
+	multiaddr.StringCast("/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa"),
+	multiaddr.StringCast("/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb"),
+	multiaddr.StringCast("/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt"),
+	multiaddr.StringCast("/ip4/104.131.131.82/tcp/4001/p2p/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ"),
+}
+
+const Protocol = "/libp2p-port-forward/v0"
