f39079f0d108a57dddae06620dda133b55a6a095
Haruki Nazawa <nazawaharuki@gmail.com>
2021-02-09 18:07:44 +0900

    DHT上でPeerRoutingができるようにした

--- main.go
+++ main.go
@@ -7,9 +7,10 @@ import (
 	"os/signal"
 
 	"github.com/libp2p/go-libp2p-core/network"
+	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/studiokaiji/libp2p-port-forward/client"
 	"github.com/studiokaiji/libp2p-port-forward/cmd"
 	"github.com/studiokaiji/libp2p-port-forward/server"
-	"github.com/studiokaiji/libp2p-port-forward/util"
 )
 
 func main() {
@@ -17,15 +18,27 @@ func main() {
 
 	o := cmd.FlagOptions
 	ctx := context.Background()
-	
+
 	if len(o.ConnectTo) == 0 {
 		s := server.New(ctx, "127.0.0.1", o.AcceptPort)
 		s.Listen(func(stream network.Stream) {
-			fmt.Println(s.ID)
+			fmt.Println(stream.ID())
 		})
 	} else {
-		fmt.Println("AAAAA")
+		pid, err := peer.IDB58Decode(o.ConnectTo)
+		if err != nil {
+			fmt.Println(pid.String())
+			panic(err)
+		}
+
+		c := client.New(ctx, "127.0.0.1", o.ForwardPort)
+		fmt.Println("Started client node.")
+
+		c.Connect(ctx, pid)
 	}
 
-	<-util.OSInterrupt()
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+
+	<-quit
 }
--- server/server.go
+++ server/server.go
@@ -4,11 +4,15 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"sync"
 
 	libp2p "github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p-core/network"
 	"github.com/libp2p/go-libp2p-core/peer"
+	discovery "github.com/libp2p/go-libp2p-discovery"
 	host "github.com/libp2p/go-libp2p-host"
+	dht "github.com/libp2p/go-libp2p-kad-dht"
+	"github.com/studiokaiji/libp2p-port-forward/constants"
 )
 
 type Server struct {
@@ -17,6 +21,8 @@ type Server struct {
 	port uint16
 	ID   peer.ID
 }
+
+var idht *dht.IpfsDHT
 
 // New create server
 func New(ctx context.Context, addr string, port uint16) *Server {
@@ -31,7 +37,44 @@ func New(ctx context.Context, addr string, port uint16) *Server {
 
 // Listen when it receives a value from the other node, and calls the handler.
 func (s *Server) Listen(handler network.StreamHandler) {
-	log.Println("Waiting for client to connect.")
-	s.node.SetStreamHandler("/libp2p-port-forward/v0", handler)
+	ctx := context.Background()
+
+	log.Println("Announcing ourselves...")
+
+	kademliaDHT, err := dht.New(ctx, s.node)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println("Bootstrapping the DHT...")
+
+	if err = kademliaDHT.Bootstrap(ctx); err != nil {
+		log.Fatalln(err)
+	}
+
+	var wg sync.WaitGroup
+	for _, peerAddr := range constants.BootstrapPeers {
+		peerinfo, _ := peer.AddrInfoFromP2pAddr(peerAddr)
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			if err := s.node.Connect(ctx, *peerinfo); err != nil {
+				log.Fatalln(err)
+			} else {
+				log.Println("Connection established with bootstrap node:", *peerinfo)
+			}
+		}()
+	}
+	wg.Wait()
+
+	routingDiscovery := discovery.NewRoutingDiscovery(kademliaDHT)
+	discovery.Advertise(ctx, routingDiscovery, s.ID.Pretty())
+
+	log.Println("Successfully announced.")
+
+	s.node.SetStreamHandler(constants.Protocol, handler)
+
+	log.Println("Waiting for client to connect.\nYour PeerId is", s.ID.Pretty())
+
 	return
 }
