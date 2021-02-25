7b721b8121ccf10c95a18d00c0b5ec6bf18c752a
Haruki Nazawa <nazawaharuki@gmail.com>
2021-02-25 22:18:14 +0900

    共通部分の切り出しにより不要になったコードを削除

--- client/client.go
+++ client/client.go
@@ -5,20 +5,16 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"sync"
 
-	libp2p "github.com/libp2p/go-libp2p"
-	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-core/network"
 	"github.com/libp2p/go-libp2p-core/peer"
-	discovery "github.com/libp2p/go-libp2p-discovery"
 	dht "github.com/libp2p/go-libp2p-kad-dht"
-	"github.com/studiokaiji/libp2p-port-forward/constants"
+	"github.com/studiokaiji/libp2p-port-forward/libp2p"
 	"github.com/studiokaiji/libp2p-port-forward/util"
 )
 
 type Client struct {
-	node host.Host
+	node libp2p.Node
 	addr string
 	port uint16
 }
@@ -26,13 +22,10 @@ type Client struct {
 var idht *dht.IpfsDHT
 
 func New(ctx context.Context, addr string, port uint16) Client {
-	libp2pAddr := libp2p.ListenAddrStrings("/ip4/127.0.0.1/tcp/0")
-	node, err := libp2p.New(ctx, libp2pAddr)
+	node, err := libp2p.New(ctx)
 	if err != nil {
 		log.Fatalln(err)
 	}
-
-	fmt.Println("Started client node.")
 
 	return Client{node, addr, port}
 }
@@ -40,7 +33,7 @@ func New(ctx context.Context, addr string, port uint16) Client {
 func (c *Client) ListenAndSync(stream network.Stream) {
 	log.Println("Creating listen server")
 
-  	tcpAddr, err := net.ResolveTCPAddr("tcp", fmt.Sprintf("%s:%d", c.addr, c.port))
+	tcpAddr, err := net.ResolveTCPAddr("tcp", fmt.Sprintf("%s:%d", c.addr, c.port))
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -64,70 +57,7 @@ func (c *Client) ListenAndSync(stream network.Stream) {
 }
 
 func (c *Client) Connect(ctx context.Context, targetPeerId peer.ID) network.Stream {
-	peer := c.discoveryPeer(ctx, targetPeerId)
-
-	log.Println("Connecting to", peer.ID)
-
-	stream, err := c.node.NewStream(ctx, peer.ID, constants.Protocol)
-	if err != nil {
-		log.Println(err)
-		return nil
-	}
-	log.Println("Connected to", peer.ID)
-
-	return stream
-}
-
-func (c *Client) discoveryPeer(ctx context.Context, targetPeerId peer.ID) peer.AddrInfo {
-	kademliaDHT, err := dht.New(ctx, c.node)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	if err = kademliaDHT.Bootstrap(ctx); err != nil {
-		log.Fatalln(err)
-	}
-
-	var wg sync.WaitGroup
-	for _, peerAddr := range constants.BootstrapPeers {
-		peerinfo, _ := peer.AddrInfoFromP2pAddr(peerAddr)
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			if err := c.node.Connect(ctx, *peerinfo); err != nil {
-				log.Fatalln(err)
-			} else {
-				log.Println("Connection established with bootstrap node:", *peerinfo)
-			}
-		}()
-	}
-	wg.Wait()
-
-	routingDiscovery := discovery.NewRoutingDiscovery(kademliaDHT)
-	peerChan, err := routingDiscovery.FindPeers(ctx, targetPeerId.Pretty())
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	var targetPeer peer.AddrInfo
-
-	for peer := range peerChan {
-		if peer.ID == c.node.ID() {
-			continue
-		}
-
-		if peer.ID == targetPeerId {
-			log.Println("Found peer:", peer.ID)
-			targetPeer = peer
-			break
-		}
-	}
-
-	if len(targetPeer.ID) == 0 {
-		log.Fatalln("Peer not found.")
-	}
-
-	return targetPeer
+	return c.node.ConnectToTargetPeer(ctx, targetPeerId)
 }
 
 /*
